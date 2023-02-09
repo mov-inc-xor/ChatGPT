6e0d0f49ec1933c45a8a52ed42e2e5770e28299c
Илья Артёменко <mov.inc.xor@gmail.com>
2023-02-09 04:55:32 +0300

    Initial Commit

---
+++ ChatGPT/ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTApp.swift
+//  ChatGPT
+//
+//  Created by Илья on 09.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
